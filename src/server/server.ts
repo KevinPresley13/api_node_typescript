46f3ca6311a2d00ae190e1e7968da577f49280f0
Kevin Presley <kevinpresley1995@gmail.com>
2023-09-08 20:14:53 -0300

    feat: rota de erro pagina not found

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,5 +8,8 @@ server.use(testeController);
 server.get("/ping", (req: Request, res: Response)=>{
     return res.send({resposta: "pong"});
 });
-
+server.use((req: Request, res: Response)=>{
+    res.status(404);
+    res.json({erro:"pagina nao encontrada"});
+});
 export {server};
