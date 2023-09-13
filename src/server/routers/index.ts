a072c4e140397b181747ed8a50a0202e20da417c
Kevin Presley <kevinpresley1995@gmail.com>
2023-09-13 19:05:10 -0300

    feat: excluindo arquivo de teste

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
-import * as TesteController from "../../controllers/testeController";
-
 
 const router = Router();
 
-
-router.get("/home",TesteController.home);
 
 export default router;
