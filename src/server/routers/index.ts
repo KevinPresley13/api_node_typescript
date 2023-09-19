7e95450278f5e34761504d77169f2a09101308ad
Kevin Presley <kevinpresley1995@gmail.com>
2023-09-19 20:20:25 -0300

    feat: correçao de rota

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as rotasController from "../../controllers/testeController";
+import * as rotasController from "../controllers/testeController";
 
 
 const router = Router();
