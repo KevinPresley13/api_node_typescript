d2bb5cdc530431688d31e5b6b60afeb9b83dd641
Kevin Presley <kevinpresley1995@gmail.com>
2023-10-23 22:54:57 -0300

    feat: criando rota nova cidade

---
+++ src/server/controllers/cidades/Create.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+interface ICidade {
+    nome: string
+}
+
+export const create = (req:Request<{}, {}, ICidade>, res: Response) => {
+    const data: ICidade = req.body;
+
+
+    return res.json({data});
+};
--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as rotasController from "../controllers/testeController";
+import { cidadesControler } from "./../controllers";
 
 
 const router = Router();
@@ -7,6 +8,6 @@ const router = Router();
 router.get("/teste",rotasController.teste);
 router.post("/testepost",rotasController.testePost);
 
-
+router.post("/newcit",cidadesControler.create);
 
 export default router;
