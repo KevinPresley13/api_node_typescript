b09c35365618afb781b0437ed6341644c01ed5b6
Kevin Presley <kevinpresley1995@gmail.com>
2023-10-26 21:34:39 -0300

    feat: primeira versao de validaçao via yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express";
+import * as yup from "yup";
+
 
 interface ICidade {
-    nome: string
+    nome: string,
 }
 
-export const create = (req:Request<{}, {}, ICidade>, res: Response) => {
-    const data: ICidade = req.body;
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+
+});
+
+export const create  = async (req:Request<{}, {}, ICidade>, res: Response) => {
+    let validateData: ICidade | undefined = undefined;
+    
+    try {
+        validateData = await bodyValidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.json({
+            errors: {
+                default: yupError.message,
+            }
+        });
+    }
 
 
-    return res.json({data});
+    return res.json({validateData});
 };
