1e5dfcefe0e676f302823801a421e60bc664cddc
Kevin Presley <kevinpresley1995@gmail.com>
2023-10-27 20:12:35 -0300

    feat: validando mais de um campo no yup

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -8,22 +8,24 @@ interface ICidade {
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
     nome: yup.string().required().min(3),
-
+    estado: yup.string().required().min(3)
 });
 
 export const create  = async (req:Request<{}, {}, ICidade>, res: Response) => {
     let validateData: ICidade | undefined = undefined;
     
     try {
-        validateData = await bodyValidation.validate(req.body);
-    } catch (error) {
-        const yupError = error as yup.ValidationError;
+        validateData = await bodyValidation.validate(req.body, {abortEarly: false});
+    } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const errors: Record<string, string> = {};
 
-        return res.json({
-            errors: {
-                default: yupError.message,
-            }
+        yupError.inner.forEach(error => {
+            if(error.path === undefined) return;
+            errors[error.path] = error.message;
         });
+
+        return res.status(400).json({errors});
     }
 
 
